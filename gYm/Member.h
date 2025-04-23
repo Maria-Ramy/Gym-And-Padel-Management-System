ddcf9ef5dea09cd22856ec3dcbb36600d6fe5da3
Youssef <zcst010@gmail.com>
2025-04-23 20:17:44 +0200

    done

--- gYm/Member.h
+++ gYm/Member.h
@@ -2,9 +2,9 @@
 #include"Person.h"
 #include"Subscription.h"
 
+
 class Member : public Person
 {
-private:
 	Subscription plan;
 	int ID;
 public:
--- gYm/Subscription.h
+++ gYm/Subscription.h
@@ -2,7 +2,7 @@
 #include<iostream>
 using namespace std;
 
-// Basic, Standard, Premium, 
+// Basic, Standard, Premium, Ultimate 
 class Subscription
 {
 private:
